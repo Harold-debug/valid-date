220d06850190f3d84870259f23bfe935a40ef795
Harold-debug <65024481+Harold-debug@users.noreply.github.com>
2021-03-30 23:24:46 +0100

    Add files via upload

---
+++ Date.c
@@ -0,0 +1,28 @@
+/* 
+Date.c*/
+#include<stdio.h>
+int main()
+{
+	int dd,mm,yy;
+	printf("enter date (DD/MM/YYYY format): ");
+	scanf("%d/%d/%d",&dd,&mm,&yy);
+	if(yy>=1000 && yy<=1999){
+		if(mm>=1 && mm<=12){
+		if((dd>=1 &&dd<=31) && (mm==1||mm==3||mm==5||mm==7||mm==8||mm==10||mm==12))
+		printf("Date is valid.\n");
+		else if ((dd>=1 &&dd<=31)&&(mm==4||mm==6||mm==9||mm==11))
+		printf("Date is valid.\n");
+		else if ((dd>=1&&dd<=28)&&(mm==2))
+		printf("Date is valid.");
+		else if (dd==29&&mm==2&&(yy%400==0 ||(yy%4==0 && yy%100!=0)))
+		printf("Day is invalid.\n");
+		else printf("Date is invalid.\n");
+	}
+	else{ printf("Month is invalid.\n");
+	}
+	}
+
+else{printf("Year is not valid.\n");
+}
+return 0;
+}
